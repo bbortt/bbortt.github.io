35aaf067aded2f62e47c888b3bcdb272c5721c37
bbortt <timon.borter@post.ch>
2018-12-09 19:14:36 +0100

    add homepage.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 // @flow
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+
+import Home from './view/Home';
+
 import './App.scss';
 
 type Props = { /* ... */ };
@@ -8,21 +10,10 @@ type Props = { /* ... */ };
 class App extends Component<Props> {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='App'>
+        <div className='container'>
+          <Home/>
+        </div>
       </div>
     );
   }
---
+++ src/view/Home.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+import './Home.scss';
+
+type Props = { /* ... */ };
+
+class Home extends Component<Props> {
+  render() {
+    return (
+      <h1>Hello World</h1>
+    );
+  }
+}
+
+export default Home;
