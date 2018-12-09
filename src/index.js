ab1527af137e9104ee1a4ef39437a266c98b60ba
bbortt <timon.borter@post.ch>
2018-12-09 18:59:29 +0100

    Add bootstrap and reactstrap.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
+
 import App from './App';
+
+import './index.scss';
+import 'bootstrap/dist/js/bootstrap.min';
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
