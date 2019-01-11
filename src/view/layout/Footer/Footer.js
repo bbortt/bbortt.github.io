1727618208ec31cadea87546554c359e5ce51d98
bbortt <timon.borter@gmx.ch>
2019-01-11 14:12:31 +0100

    little footer correcture.

--- src/view/layout/Footer/Footer.js
+++ src/view/layout/Footer/Footer.js
@@ -11,7 +11,7 @@ class Footer extends Component<Props> {
       <footer className='Footer'>
         <nav className="navbar navbar-dark bg-dark mt-5 fixed-bottom">
           <div className="m-auto navbar-text">
-            <span>Hosted Via <a href='https://pages.github.com/'><bold>GitHub</bold> Pages</a></span>
+            <span>Hosted On <a href='https://pages.github.com/'><bold>GitHub</bold> Pages</a></span>
           </div>
         </nav>
       </footer>
