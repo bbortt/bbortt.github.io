5b5aa897b6212d8e236a95810febae0dc0f8fadb
Timon Borter <timon.borter@gmx.ch>
2021-03-22 17:28:30 +0100

    fix gpg download.

--- src/container/key-list/index.js
+++ src/container/key-list/index.js
@@ -1,11 +1,13 @@
 // @flow
 import React, {Component} from 'react';
 
-import * as key from '../../keys/timon_borter.asc';
-
 type Props = { /* ... */ };
 
 class KeyList extends Component<Props> {
+  downloadFile = () => {
+    window.location.href = ''
+  }
+
   render() {
     return (
       <div className='key-list'>
@@ -14,8 +16,9 @@ class KeyList extends Component<Props> {
         </h2>
 
         <p>
-          My GPG Fingerprint: <a href={key} className='text-underline'>624D F4DC 4729 9F8C 273A CDF3 EA08 2DF7
-          4988 780F</a>.
+          My GPG Fingerprint: <a
+          href='https://raw.githubusercontent.com/bbortt/bbortt.github.io/development/src/keys/timon_borter.asc'
+          download className='text-underline'>624D F4DC 4729 9F8C 273A CDF3 EA08 2DF7 4988 780F</a>.
         </p>
       </div>
     );
