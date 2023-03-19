3e43626c83cc5fc24c39e76362534379a9ba825b
Timon Borter <timon.borter@gmx.ch>
2023-03-19 10:00:35 +0100

    chore: move navbar into container

--- src/container/layout/header/index.js
+++ src/container/layout/header/index.js
@@ -11,23 +11,23 @@ type Props = { /* ... */ };
 export const Header: Component<Props> = () => (
   <div className="header">
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <NavLink to="/" className="nav-link navbar-brand">
-        <h1>bbortt</h1>
-      </NavLink>
-
-      <button className="navbar-toggler" type="button" data-toggle="collapse"
-              data-target="#navbarTogglerDemo02"
-              aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-
-      <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-        <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-          <MaybeActiveLink href="/" text="Home"/>
-          <MaybeActiveLink href="/projects" text="Projects"/>
-          <MaybeActiveLink href="/social" text="Social"/>
-          <MaybeActiveLink href="/pgp" text="PGP"/>
-        </ul>
+      <div className='container'>
+        <NavLink to="/" className="nav-link navbar-brand">
+          <h1>bbortt</h1>
+        </NavLink>
+        <button className="navbar-toggler" type="button" data-toggle="collapse"
+                data-target="#navbarTogglerDemo02"
+                aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+          <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+            <MaybeActiveLink href="/" text="Home"/>
+            <MaybeActiveLink href="/projects" text="Projects"/>
+            <MaybeActiveLink href="/social" text="Social"/>
+            <MaybeActiveLink href="/pgp" text="PGP"/>
+          </ul>
+        </div>
       </div>
     </nav>
   </div>
